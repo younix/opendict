6256a8352d283dd121547e049a9aed43c74f5553
Jan Klemkow <j.klemkow@wemelug.de>
2023-10-18 23:03:42 +0200

    rename myr to res[ults]

--- main.c
+++ main.c
@@ -88,7 +88,7 @@ main(int argc, char *argv[])
 {
 	struct dc_database mydb;
 	struct dc_index_list list;
-	struct dc_index_entry *myr;
+	struct dc_index_entry *res;
 	char *db_path = NULL, *idx_path = NULL;
 	char *lookup;
 	int ch, i, r = 0;
@@ -133,10 +133,10 @@ main(int argc, char *argv[])
 		err(1, "pledge");
 
 	SLIST_INIT(&list);
-	if ((myr = calloc(MAX_RESULTS, sizeof(struct dc_index_entry))) == NULL)
+	if ((res = calloc(MAX_RESULTS, sizeof(struct dc_index_entry))) == NULL)
 		err(1, "calloc");
 	for (i = 0; i < MAX_RESULTS; i++)
-		SLIST_INSERT_HEAD(&list, &myr[i], entries);
+		SLIST_INSERT_HEAD(&list, &res[i], entries);
 
 	if (database_open(db_path, &mydb) == -1)
 		errx(1, "database_open");
